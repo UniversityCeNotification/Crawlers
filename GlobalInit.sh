64b84c7277529791e13ab051d251237356e81144
pleycpl <h4rvey@protonmail.com>
2017-05-27 20:27:09 +0000

    creating .env file

--- GlobalInit.sh
+++ GlobalInit.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+read -p 'There is .env file in crawler?(y/n) : ' status
+if [[ "$status" = "n" ]]; then
+  read -p "MongoDbUrl:" url
+  read -p "MongoDbName:" name
+  echo "MongoDbUrl='$url'" > .env
+  echo "MongoDbName='$name'" >> .env
+fi
+
 runAllCrawlers() {
   for i in $(ls -d ./*/init.sh)
   do
