adbe4cce02999f0dad882a6c11f1803e2a05d158
pleycpl <h4rvey@protonmail.com>
2017-05-27 15:29:08 +0000

    schedule all crawlers

--- GlobalInit.sh
+++ GlobalInit.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for i in $(ls -d ./*/init.sh)
+runAllCrawlers() {
+  for i in $(ls -d ./*/init.sh)
+  do
+      echo ${i%%/init.sh}
+      cd ${i%%/init.sh}
+      bash init.sh
+      cd -
+  done
+}
+
+while true
 do
-    echo ${i%%/init.sh}
-    cd ${i%%/init.sh}
-    bash init.sh
-    cd -
+  runAllCrawlers
+  sleep 10m
 done
